f282688fc15c8e51dfe29637297d50c98cbcdc92
nocdn <bartek.bak@protonmail.com>
2025-12-07 12:14:12 +0000

    feat: remove react scan from prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,11 +54,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
+        {/* <script
           crossOrigin="anonymous"
           src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
-        {/* rest of your scripts go under */}
+        /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${rounded.variable}`}
